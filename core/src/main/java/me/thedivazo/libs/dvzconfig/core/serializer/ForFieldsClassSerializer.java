a56e07a929c555ecdb688fdb68f29b7d048f090e
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-06-11 01:05:01 +0300

    Создание класса ForFieldsClassSerializer для сериализации по наименованию полей

---
+++ core/src/main/java/me/thedivazo/libs/dvzconfig/core/serializer/ForFieldsClassSerializer.java
@@ -0,0 +1,92 @@
+package me.thedivazo.libs.dvzconfig.core.serializer;
+
+import com.google.common.collect.Maps;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.objectmapping.ObjectMapper;
+import org.spongepowered.configurate.serialize.SerializationException;
+import org.spongepowered.configurate.serialize.TypeSerializer;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class ForFieldsClassSerializer<T> implements TypeSerializer<T> {
+    private final Class<T> parentClass;
+    private final Map<Class<? extends T>, Map<String, Class<?>>> classFields;
+
+
+    public ForFieldsClassSerializer(Class<T> parentClass, Set<Class<? extends T>> classes) {
+        this.parentClass = parentClass;
+        this.classFields = classes.stream()
+                .collect(
+                        Collectors.toMap(Function.identity(), clazz ->
+                                Arrays.stream(clazz.getFields())
+                                        .collect(Collectors.toMap(Field::getName, Field::getType))
+                        )
+                );
+    }
+
+    private static boolean isClass(ConfigurationNode node, Map<String, Class<?>> fields) {
+        for (Map.Entry<String, Class<?>> entry : fields.entrySet()) {
+            String key = entry.getKey();
+            Class<?> expectedClass = entry.getValue();
+
+            ConfigurationNode child = node.node(key);
+
+            if (child.virtual() || child.raw() == null) {
+                return false;
+            }
+
+            Object rawValue = child.raw();
+
+            if (!expectedClass.isInstance(rawValue)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public T deserialize(Type type, ConfigurationNode node) throws SerializationException {
+        if (!(type instanceof Class<?>) || !parentClass.isAssignableFrom((Class<?>) type)) {
+            throw new SerializationException("Expected " + parentClass + " or children, but got " + type);
+        }
+
+        Map<Class<? extends T>, Map<String, Class<?>>> findedClass = Maps.filterValues(classFields, fields -> isClass(node, fields));
+        if(findedClass.size() != 1) throw new SerializationException("Expected exactly one class, but got " + findedClass.size());
+
+        Class<? extends T> finalyClass = findedClass.keySet().iterator().next();
+
+        return ObjectMapper.factory().get(finalyClass).load(node);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public void serialize(Type type, @Nullable T obj, ConfigurationNode node) throws SerializationException {
+        if (obj == null)
+            throw new SerializationException("Could not serialize object, because object is null");
+
+        if (!parentClass.isInstance(obj)) {
+            throw new SerializationException("Expected instance of " + parentClass.getName() + ", but got " + obj.getClass().getName());
+        }
+
+        Class<?> objClass = obj.getClass();
+
+        if (!classFields.containsKey(objClass)) {
+            throw new SerializationException("Unknown class for serialization: " + objClass.getName());
+        }
+
+        try {
+            ObjectMapper<T> mapper = (ObjectMapper<T>) ObjectMapper.factory().get(objClass);
+            mapper.save(obj, node);
+        } catch (Exception e) {
+            throw new SerializationException(e);
+        }
+    }
+}
